f8cbacb5531db5563f29786713764a3550c82bf1
NickBarkerDev <nickthedevguy@gmail.com>
2023-03-15 02:21:44 -0700

    changed dummy data in App.jsx props

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <h1>People Persons</h1>
       <div className="all-people">
         <PersonCard firstName={"Nick"} lastName={"Barker"} age={33} hairColor={"blonde"} />
-        <PersonCard firstName={"Leo"} lastName={"Nidas"} age={10} hairColor={"spotted"} />
+        <PersonCard firstName={"King"} lastName={"Leonidas"} age={10} hairColor={"spotted"} />
         <PersonCard firstName={"Richard"} lastName={"Rinkle"} age={69} hairColor={"brown"} />
         <PersonCard firstName={"Robyn"} lastName={"Banks"} age={42} hairColor={"red"} />
       </div>
