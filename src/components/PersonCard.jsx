9a8f6d22c6bd0c41e17fca9a150a93e475396396
NickBarkerDev <nickthedevguy@gmail.com>
2023-03-15 01:48:28 -0700

    added/styled button to increment the age of each PersonCard element

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PersonCard.css'
 
 const PersonCard = (props) => {
 	let {firstName, lastName, age, hairColor} = props
+	const [personAge, setPersonAge] = useState(age)
+
+	const makeYouOld = () => {
+		setPersonAge(prevAge => prevAge + 1)
+	}
 
 	return (
 		<div className="hooman">
 			<h2>{firstName} {lastName}</h2>
-			<p>Age: {age}</p>
+			<p>Age: {personAge}</p>
 			<p>Hair Color: {hairColor}</p>
+			<button onClick={makeYouOld}>{firstName}'s Birthday Button</button>
 		</div>
 	);
 }
